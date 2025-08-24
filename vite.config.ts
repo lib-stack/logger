398c53b60ab9edf5d4d521bfb156002e8fea0c5d
Kisstar <dwh.chn@foxmail.com>
2025-08-24 17:31:03 +0800

    build: support for building and testing

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+// https://vite.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  plugins: [
+    dts({ outDir: 'dist/types' }),
+  ],
+  build: {
+    lib: {
+      formats: ['es'],
+      entry: {
+        logger: './src/main.ts',
+      },
+      fileName: (format, name) => `${name}.${format}.js`,
+    },
+  },
+})
