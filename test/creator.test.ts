0ce0bd4f4072c1f9e4e30ef03e5b521d039a272d
Kisstar <dwh.chn@foxmail.com>
2025-08-31 13:07:37 +0800

    test: supplementary test cases

--- test/creator.test.ts
+++ test/creator.test.ts
@@ -1,14 +1,51 @@
 import { describe, expect, it, vi } from 'vitest'
-import createLogger from '../src/creator'
+import { createLogger, logger } from '../src/creator'
 
 describe('createLogger', () => {
+  it('should get default logger', () => {
+    const defaultLogger = logger.get()
+
+    expect(defaultLogger).toBe(logger)
+  })
+
   it('should create a logger with custom name', () => {
-    const logger = createLogger({ name: 'test' })
+    // the default logger
+    logger.setLevel('info')
+
     const info = vi.spyOn(console, 'info')
 
-    logger.setLevel('info')
     logger.info('some message')
-    expect(info.mock.calls[0].includes('[test]')).toBeTruthy()
+    expect(info.mock.calls[0].includes('[default]')).toBeTruthy()
+
+    // Use the `get` method to obtain the logger
+    const moduleLogger = logger.get('module')
+    const moduleInfo = vi.spyOn(console, 'info')
+
+    moduleLogger.setLevel('info')
+    moduleLogger.info('some message')
+    expect(moduleInfo.mock.calls[0].includes('[module]')).toBeTruthy()
+
+    // Use the `createLogger` method to obtain the logger
+    const customLogger = createLogger({ name: 'create' })
+    const customInfo = vi.spyOn(console, 'info')
+
+    customLogger.setLevel('info')
+    customLogger.info('some message')
+    expect(customInfo.mock.calls[0].includes('[create]')).toBeTruthy()
+  })
+
+  it('should get same logger by same name', () => {
+    const logger1 = logger.get('same')
+    const logger2 = logger.get('same')
+
+    expect(logger1).toBe(logger2)
+  })
+
+  it('should get different logger by same name', () => {
+    const logger1 = createLogger({ name: 'same' })
+    const logger2 = createLogger({ name: 'same' })
+
+    expect(logger1).not.toBe(logger2)
   })
 
   it('should set and get log level', () => {
@@ -58,5 +95,12 @@ describe('createLogger', () => {
     expect(warnSpy.mock.calls[0].join(' ')).toMatch(/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[WARN\] \[\w+\] - warning message/)
     logger.error('error message')
     expect(errorSpy.mock.calls[0].join(' ')).toMatch(/\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[ERROR\] \[\w+\] - error message/)
+  })
+
+  it('should get all loggers', () => {
+    const logger = createLogger({ name: 'logger1' })
+
+    logger.get('logger2')
+    expect(logger.getAll().length).toBe(2)
   })
 })
